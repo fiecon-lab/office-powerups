96d5be3374f6d7638a0706f4cdec820d31a1dad3
Tom Wright <tom.wright@fiecon.com>
2025-01-30 12:38:25 +0000

    another fix

--- taskpane.js
+++ taskpane.js
@@ -1374,13 +1374,6 @@ const fillSelection = async () =>
 
 const applyQCFill = async (range, context) =>
   tryCatch(async () => {
-    range.load(["rowCount", "columnCount"]);
-    await context.sync();
-
-    const n_cells = range.rowCount * range.columnCount;
-
-    if (n_cells > 1000) throw new Error("Cannot apply fill to more than 1000 cells at a time.");
-
     // assign colour to range
     let [result, error] = await getFromLocalStorage("pwrups_fill_col");
     if (!error) range.format.fill.color = result;
